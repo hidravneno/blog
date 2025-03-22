06d34eefbad1087391960a54bad41798715eaf56
Hidravneno <aramburoeduardo972@gmail.com>
2025-03-22 15:31:16 -0700

    server

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["127.0.0.1" , "localhost", ".herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "limitless-springs-45455.herokuapp.com", ".herokuapp.com"]
 
 
 # Application definition
